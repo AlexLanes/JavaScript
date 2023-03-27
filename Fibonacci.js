377001fd45ffff06bb53aab528d4df962030ccaa
Alex Lanes <94094017+AlexLanes@users.noreply.github.com>
2023-03-27 10:19:51 -0300

    Create Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,31 @@
+/** 
+ * Cache do fibonacci 
+ */
+let memoization = {
+    "1": 0,
+    "2": 1
+}
+
+/**
+ * Calcular o N-ésimo número do fibonacci
+ * @param {Number} n Número do fibonacci
+ */
+function fibonacci( n ){
+    // Exceção
+    if( n <= 0 ) 
+        throw new Error( "N-ésimo número não pode ser menor ou igual a 0" )
+    // valor não cacheado
+    else if( memoization[n] === undefined ) 
+        memoization[ n ] = fibonacci( n - 1 ) + fibonacci( n - 2 )
+
+    return memoization[ n ]
+}
+
+const N_ÉSIMO = 45
+for( let n = 1; n <= N_ÉSIMO; n++ )
+    console.log( `Resultado do fibonacci "${n}": ${fibonacci(n)}` )
+
+/**
+ * Abra um terminal
+ * Digite "npm start"
+ */
